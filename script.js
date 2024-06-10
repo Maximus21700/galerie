becaab35daa8d318a8f186cadef71a56b8389e03
Maximus21700 <154612458+Maximus21700@users.noreply.github.com>
2024-06-10 09:51:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    changeTab('accueil');
-  });
-  
-  function changeTab(tabName) {
-    const tabs = document.querySelectorAll('.content');
-    tabs.forEach(tab => {
-      tab.style.display = 'none';
-    });
-  
-    document.getElementById(tabName).style.display = 'block';
-  }
-// Ajoutez ces lignes dans la fonction changeTab
-function changeTab(tabName) {
-    const tabs = document.querySelectorAll('.content');
-    tabs.forEach(tab => {
-      tab.style.display = 'none';
-    });
-  
-    const currentTab = document.getElementById(tabName);
-    currentTab.style.display = 'block';
-  
-    // Ajoutez la classe "shake" à l'image sur la page d'accueil
-    if (tabName === 'accueil') {
-      const accueilImage = currentTab.querySelector('img');
-      accueilImage.classList.add('shake');
-    }
-  }
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Bienvenue à la Galerie d'Art");
+});
